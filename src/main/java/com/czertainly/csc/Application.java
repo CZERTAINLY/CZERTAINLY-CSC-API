56edcbb3e8adde5aac3cf331ae411196d89eb182
3keyroman <roman.cinkais@3key.company>
2025-04-25 10:36:31 +0200

    Immediately delete key after one-time signing operation

--- src/main/java/com/czertainly/csc/Application.java
+++ src/main/java/com/czertainly/csc/Application.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @ConfigurationPropertiesScan("com.czertainly.csc.configuration")
 @EnableRetry
+@EnableAsync
 public class Application {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/czertainly/csc/configuration/AsyncConfig.java
@@ -0,0 +1,23 @@
+package com.czertainly.csc.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class AsyncConfig {
+
+    @Bean(name = "oneTimeKeyExecutor")
+    public Executor oneTimeKeyExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(5);
+        executor.setQueueCapacity(50);
+        executor.setThreadNamePrefix("OneTimeKeyAsync-");
+        executor.initialize();
+        return executor;
+    }
+
+}
